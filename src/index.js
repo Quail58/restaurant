f1ddaee24949bf83c7a457929de371a4198a5806
John Gibson <jhngbsn09@gmail.com>
2023-02-09 14:43:09 -0500

    Simplified source javascript file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import './style.css';
-import Food from './assets/food.png';
-import homePage from 'sr./mainpage.js';
+import home from './mainpage.js';
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Food;
-
-  element.appendChild(myIcon);
-
-mainPage();
+home();
