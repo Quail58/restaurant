84693feb3ff6dd9b78cb6fba07a85025ca901ee9
John Gibson <jhngbsn09@gmail.com>
2023-02-11 18:49:37 -0500

    Remove chef Image

--- src/about.js
+++ src/about.js
@@ -2,6 +2,9 @@ function aboutPage() {
     //creates the text section
     const about1 = document.getElementById('about1').innerHTML = "Thank you for taking interest in our restaurant";     
     const about2 = document.getElementById('about2').innerHTML = "We dont want to reveal too much about our space.<br>Each visit is its own experience.";
+
+    const removeChef = document.getElementById('chefKiss');
+    removeChef.parentNode.removeChild(removeChef);
 }
 
 export default aboutPage;
--- src/mainpage.js
+++ src/mainpage.js
@@ -2,6 +2,9 @@ function home() {
         //sets the innerHTML for the home section 
         const about1 = document.getElementById('about1').innerHTML = "Eclectic Food From Across The Globe";     
         const about2 = document.getElementById('about2').innerHTML = "Tables available by reservation only<br>To make a reservation please call 800-888-1234";
+
+        const removeChef = document.getElementById('chefKiss');
+        removeChef.parentNode.removeChild(removeChef);
 }
 
 export default home;
