25bb8cf84ee39a4f01a6636ece7e97b241fd46d2
John Gibson <jhngbsn09@gmail.com>
2023-02-11 18:40:33 -0500

    Added in chefkiss Logo

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,11 @@ function menuPage() {
         const about1 = document.getElementById('about1').innerHTML = "Our Menu is curated daily by Chef Doug Gibs";     
         const about2 = document.getElementById('about2').innerHTML = "We can change the courses based on dietary restrictons if you let us know beforehand";
 
-        const about = getElementById
+        const about = document.getElementById('about');
+        const chefKiss = document.createElement('img');
+        chefKiss.setAttribute('id','chefKiss')
+        chefKiss.src = './assets/chef.jpg';
+        about.appendChild(chefKiss);
 }
 
 export default menuPage;
