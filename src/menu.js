5744fcfc255cacd785d432edeaf1ce40421d352f
John Gibson <jhngbsn09@gmail.com>
2023-02-11 18:01:49 -0500

    Added function for menu tab

---
+++ src/menu.js
@@ -0,0 +1,7 @@
+function menuPage() {
+        //creates the text section
+        const about1 = document.getElementById('about1').innerHTML = "Our Menu is curated daily by Chef Doug Gibs";     
+        const about2 = document.getElementById('about2').innerHTML = "We can change the courses based on dietary restrictons if you let us know beforehand";
+}
+
+export default menuPage;
