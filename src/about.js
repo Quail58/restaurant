3988ae0698f50cfdd89fe7d9fdc1981d7e908063
John Gibson <jhngbsn09@gmail.com>
2023-02-11 18:00:50 -0500

    Function for about tab and text

---
+++ src/about.js
@@ -0,0 +1,7 @@
+function aboutPage() {
+    //creates the text section
+    const about1 = document.getElementById('about1').innerHTML = "Thank you for taking interest in our restaurant";     
+    const about2 = document.getElementById('about2').innerHTML = "We dont want to reveal too much about our space.<br>Each visit is its own experience.";
+}
+
+export default aboutPage;
