151ffee73311c25732c0ed13acb042de68288d4f
John Gibson <jhngbsn09@gmail.com>
2023-02-11 18:01:19 -0500

    Added in imports and functions for tabs and page load

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import home from './mainpage.js';
+import menuPage from './menu.js';
+import aboutPage from './about.js';
+import header from './header.js';
 
+//runs the header and about page on load
+header();
 home();
+
+//document.getElementById('tab').onclick = aboutPage();
